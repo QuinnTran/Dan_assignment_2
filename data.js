f85433cbae29001fd32e1cd77127c6a92a5f2fdf
Quinn Tran <19821679+QuinnTran@users.noreply.github.com>
2019-10-18 22:16:41 +1100

    code format

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [
+  {
+    "id": 0,
+    "teacher": "Mr John",
+    "availability": "available"
+  },
+  {
+    "id": 1,
+    "teacher": "Mrs Bella",
+    "availability": "not available"
+  },
+  {
+    "id": 2,
+    "teacher": "Mrs Rose",
+    "availability": "not available"
+  },
+  {
+    "id": 3,
+    "teacher": "Mr Hudson",
+    "availability": "not available"
+  }
+]
---
+++ main.js
@@ -0,0 +1,37 @@
+function myFunction(obj) {
+    var showContent = obj.parentNode.getElementsByTagName("div")[0];
+    if (showContent.style.display == "none")
+        showContent.style.display = "";
+    else
+        showContent.style.display = "none";
+}
+
+// alert('working!')
+$('.button').hover(
+    //mouse enter
+    function () {
+        $('.popup').css('opacity', '1');
+        $id = $(this).attr('data-id');
+
+        //A variable for an individial movie 
+        //[] = a list of sth
+        $teacherId = data[$id];
+        // using '.' is only get name out of movie
+        $teacher = $teacherId.$teacher;
+        $availability = $teacherId.availability;
+
+        $('.teacher').text($teacher)
+        $('.availability').text($availability)
+    },
+    //mouse exits
+    function () {
+
+        $('.teacher').text('');
+        $('.availability').text('');
+        $('.popup').css('opacity', '0');
+    });
+
+// when i click on the button, change the text inside the popup to the movie 
+$('.button').click(function () {
+    // 
+});
